58714aa0493d483c6a1488dcfbe2f2224e87932d
ARIGE SRI RAM TEJA <18sriramteja@gmail.com>
2024-10-25 23:18:06 +0530

    Time: 23 ms (91.27%), Space: 17.7 MB (10.55%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a=list(i for i in s)
+        b=list(i for i in t)
+        a.sort()
+        b.sort()
+        # print(a)
+        # print(b)
+        if(a==b):
+            return True
+        else:
+            return False
